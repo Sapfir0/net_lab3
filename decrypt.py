e8dcdc16722607bb05cb677bb69e101e63384547
kyplyk <avdosev1@yandex.ru>
2019-10-21 11:24:09 +0400

    микро рефакторинг

--- decrypt.py
+++ decrypt.py
@@ -2,14 +2,8 @@ from PIL import Image
 from re import findall
 import os
 
-def decrypt(imgPath, length):
-	a = []						    
-	image = Image.open(imgPath)		
-	pix = image.load()
-
-	width = image.size[0]  		   	
-	height = image.size[1]
-
+def takeFromPixelByOneBit(pixs: Image, width, height, length: int):
+	byte_arr = []
 	curChar = 0
 	curBit = 0
 	count = 0
@@ -18,15 +12,24 @@ def decrypt(imgPath, length):
 			count +=1
 			if count >= length*8:
 				break	
-			r,g,b = pix[(x,y)]
+			r,g,b = pixs[(x,y)]
 			curChar |= (r&1)<<curBit
 			if (curBit==7):
 				curBit = 0
-				a.append(curChar)
+				byte_arr.append(curChar)
 				curChar = 0
 			else:
-				curBit+=1			
-				
+				curBit+=1	
+
+	return byte_arr
+
+def decrypt(imgPath, length):
+	a = []						    
+	image = Image.open(imgPath)		
+	pix = image.load()
+	width = image.size[0]  		   	
+	height = image.size[1]
+	a = takeFromPixelByOneBit(pix, width, height, length)			
 		
 	return ''.join([chr(elem) for elem in a])	
 
