6bc92130e1c3c5c980c167a68cdeef8e5bf38663
kyplyk <avdosev1@yandex.ru>
2019-10-22 14:38:12 +0400

    слегка изменил логику дешифрации и добавил последнюю дешифровку

--- decrypt.py
+++ decrypt.py
@@ -2,17 +2,16 @@ from PIL import Image
 from re import findall
 import os
 
-def takeFromPixelByOneBit(pixs, width, height, length: int):
+def takeFromPixelByOneBit(pixels, length: int):
 	byte_arr = []
 	curChar = 0
 	curBit = 0
 	count = 0
-	for x in range(width):
-		for y in range(height):
+	for pixel in pixels:
 			count +=1
 			if count >= length*8:
 				break	
-			r,g,b = pixs[(x,y)]
+			r,g,b = pixel
 			curChar |= (r&1)<<curBit
 			if (curBit==7):
 				curBit = 0
@@ -23,34 +22,70 @@ def takeFromPixelByOneBit(pixs, width, height, length: int):
 
 	return byte_arr
 
-def takeFromPixelByThreeBit(pixs, width, height, length:int):
-	pass
+def takeFromPixelByThreeBit(pixels, length:int):
+	byte_arr = []
+	curChar = 0
+	curBit = 0
+	count = 0
 
-def takeFromPixelByOneByte(pixs, width, height, length:int):
+	# это не лучший вариант, но одноообразного кода становится меньше
+	def writeBit():
+		nonlocal curBit, curChar, byte_arr
+		if (curBit==7):
+			curBit = 0
+			byte_arr.append(curChar)
+			curChar = 0
+		else:
+			curBit+=1
+
+	for pixel in pixels:
+		count +=1
+		if count > length*8:
+			break	
+		r,g,b = pixel
+		curChar |= (r&1)<<curBit
+		writeBit()
+		count +=1
+		if count > length*8:
+			break	
+		curChar |= (g&1)<<curBit
+		writeBit()
+		count +=1
+		if count > length*8:
+			break	
+		curChar |= (b&1)<<curBit
+		writeBit()
+		
+
+	return byte_arr
+
+def takeFromPixelByOneByte(pixels, length:int):
 	byte_arr = []
 	count = 0
-	for x in range(width):
-		for y in range(height):
-			count +=1
-			if count >= length:
-				break	
-			r,g,b = pixs[(x,y)]
-			cur_byte = 0
-			cur_byte |= r & 0x03
-			cur_byte |= (g & 0x03) << 3
-			cur_byte |= (b & 0x02) << 6
-			byte_arr.append(cur_byte) 	
+	for pixel in pixels:
+		count +=1
+		if count >= length:
+			break	
+		r,g,b = pixel
+		cur_byte = 0
+		cur_byte |= r & 0x03
+		cur_byte |= (g & 0x03) << 3
+		cur_byte |= (b & 0x02) << 6
+		byte_arr.append(cur_byte) 	
 
 	return byte_arr
 
 def decrypt(imgPath, length):
 	a = []						    
 	image = Image.open(imgPath)		
-	pix = image.load()
+	pixs = image.load()
 	width = image.size[0]  		   	
 	height = image.size[1]
 
-	a = takeFromPixelByOneBit(pix, width, height, length)			
+	# кидаем данные в одномерный массив
+	input_data = [pixs[(x,y)] for x in range(width) for y in range(height)]
+
+	a = takeFromPixelByOneBit(input_data, length)			
 		
 	return ''.join([chr(elem) for elem in a])	
 
