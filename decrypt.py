30d0a2c5696df1752e7a57f7f87c82d0de2be108
Sapfir0 <sapfir999999@yandex.ru>
2019-10-23 14:00:49 +0400

    Code style

--- decrypt.py
+++ decrypt.py
@@ -1,98 +1,97 @@
-from PIL import Image 		
+from PIL import Image
 from re import findall
 import os
 
+
 def takeFromPixelByOneBit(pixels, length: int):
-	byte_arr = []
-	curChar = 0
-	curBit = 0
-	count = 0
-	for pixel in pixels:
-		count +=1
-		if count >= length*8:
-			break	
-		r,g,b = pixel
-		curChar |= (r&1)<<curBit
-		if (curBit==7):
-			curBit = 0
-			byte_arr.append(curChar)
-			curChar = 0
-		else:
-			curBit+=1	
+    byte_arr = []
+    curChar = 0
+    curBit = 0
+    count = 0
+    for pixel in pixels:
+        count += 1
+        if count >= length * 8:
+            break
+        r, g, b = pixel
+        curChar |= (r & 1) << curBit
+        if (curBit == 7):
+            curBit = 0
+            byte_arr.append(curChar)
+            curChar = 0
+        else:
+            curBit += 1
 
-	return byte_arr
+    return byte_arr
 
-def takeFromPixelByThreeBit(pixels, length:int):
-	byte_arr = []
-	curChar = 0
-	curBit = 0
-	count = 0
 
-	# это не лучший вариант, но одноообразного кода становится меньше
-	def writeBit():
-		nonlocal curBit, curChar, byte_arr
-		if (curBit==7):
-			curBit = 0
-			byte_arr.append(curChar)
-			curChar = 0
-		else:
-			curBit+=1
+def takeFromPixelByThreeBit(pixels, length: int):
+    byte_arr = []
+    curChar = 0
+    curBit = 0
+    count = 0
 
-	for pixel in pixels:
-		count +=1
-		if count > length*8:
-			break	
-		r,g,b = pixel
-		curChar |= (r&1)<<curBit
-		writeBit()
-		count +=1
-		if count > length*8:
-			break	
-		curChar |= (g&1)<<curBit
-		writeBit()
-		count +=1
-		if count > length*8:
-			break	
-		curChar |= (b&1)<<curBit
-		writeBit()
-		
+    # это не лучший вариант, но одноообразного кода становится меньше
+    def writeBit():
+        nonlocal curBit, curChar, byte_arr
+        if curBit == 7:
+            curBit = 0
+            byte_arr.append(curChar)
+            curChar = 0
+        else:
+            curBit += 1
 
-	return byte_arr
+    for pixel in pixels:
+        count += 1
+        if count > length * 8:
+            break
+        r, g, b = pixel
+        curChar |= (r & 1) << curBit
+        writeBit()
+        count += 1
+        if count > length * 8:
+            break
+        curChar |= (g & 1) << curBit
+        writeBit()
+        count += 1
+        if count > length * 8:
+            break
+        curChar |= (b & 1) << curBit
+        writeBit()
 
-def takeFromPixelByOneByte(pixels, length:int):
-	byte_arr = []
-	count = 0
-	for pixel in pixels:
-		count +=1
-		if count >= length:
-			break	
-		r,g,b = pixel
-		cur_byte = 0
-		cur_byte |= r & 0x07
-		cur_byte |= (g & 0x07) << 3
-		cur_byte |= (b & 0x03) << 6
-		byte_arr.append(cur_byte) 	
+    return byte_arr
 
-	return byte_arr
+
+def takeFromPixelByOneByte(pixels, length: int):
+    byte_arr = []
+    count = 0
+    for pixel in pixels:
+        count += 1
+        if count >= length:
+            break
+        r, g, b = pixel
+        cur_byte = 0
+        cur_byte |= r & 0x07
+        cur_byte |= (g & 0x07) << 3
+        cur_byte |= (b & 0x03) << 6
+        byte_arr.append(cur_byte)
+
+    return byte_arr
+
 
 def decrypt(imgPath, length, mode="1b"):
-	a = []						    
-	image = Image.open(imgPath)		
-	pixs = image.load()
-	width = image.size[0]  		   	
-	height = image.size[1]
+    image = Image.open(imgPath)
+    pixs = image.load()
+    width = image.size[0]
+    height = image.size[1]
 
-	# кидаем данные в одномерный массив
-	input_data = [pixs[(x,y)] for x in range(width) for y in range(height)]
+    # кидаем данные в одномерный массив
+    input_data = [pixs[(x, y)] for x in range(width) for y in range(height)]
 
-	if mode == "1b":
-		a = takeFromPixelByOneBit(input_data, length)			
-	elif mode == "1B":
-		a = takeFromPixelByOneByte(input_data, length)	
-	else:
-		a = takeFromPixelByThreeBit(input_data, length)	
-		
-	return ''.join([chr(elem) for elem in a])	
+    if mode == "1b":
+        a = takeFromPixelByOneBit(input_data, length)
+    elif mode == "1B":
+        a = takeFromPixelByOneByte(input_data, length)
+    else:
+        a = takeFromPixelByThreeBit(input_data, length)
 
-
-
+    return ''.join([chr(elem) for elem in a])
--- encrypt.py
+++ encrypt.py
@@ -1,141 +1,143 @@
-from PIL import Image, ImageDraw 
-from random import randint		
+from PIL import Image, ImageDraw
+from random import randint
 import os
 
-def putPixelsByOneBit(data, elem):
-	"""
-	в один пиксель один значащий бит
-	"""
-	pixel_arr = []
-	currentChar = 0
-	currentBit = 0
-	count = 0
-	for pix in data:
-		if not (currentChar<len(elem)):
-			break
-		
-		r,g,b = pix
-		r = r&254
-		currentlyCodedElem = elem[currentChar]
-		code = ord(currentlyCodedElem)
-		bitToCode = code&(1<<currentBit)
-		lsb = bitToCode>>currentBit
-		r |= lsb
-		if (currentBit==7):
-			currentBit = 0
-			currentChar+=1
-		else:
-			currentBit+=1
-		pixel_arr.append((r,g,b))
 
-	return pixel_arr
+def putPixelsByOneBit(data, elem):
+    """
+    в один пиксель один значащий бит
+    """
+    pixel_arr = []
+    currentChar = 0
+    currentBit = 0
+    count = 0
+    for pix in data:
+        if not (currentChar < len(elem)):
+            break
+
+        r, g, b = pix
+        r = r & 254
+        currentlyCodedElem = elem[currentChar]
+        code = ord(currentlyCodedElem)
+        bitToCode = code & (1 << currentBit)
+        lsb = bitToCode >> currentBit
+        r |= lsb
+        if currentBit == 7:
+            currentBit = 0
+            currentChar += 1
+        else:
+            currentBit += 1
+        pixel_arr.append((r, g, b))
+
+    return pixel_arr
+
 
 def putPixelsByThreeBit(data, elem):
-	"""
-	в один пиксель три значащих бита
-	"""
-	pixel_arr = []
-	def lsb(currentlyCodedElem, currentBit, to_byte = ord):
-		code = to_byte(currentlyCodedElem)
-		bitToCode = code&(1<<currentBit)
-		return bitToCode>>currentBit
-	
-	
-	currentChar = 0
-	currentBit = 0
-	count = 0
-	
-	def nextChar():
-		nonlocal currentBit, currentChar, pixel_arr
-		if (currentBit==7):
-			currentBit = 0
-			currentChar+=1
-		else:
-			currentBit+=1
+    """
+    в один пиксель три значащих бита
+    """
+    pixel_arr = []
 
-	for pix in data:
-		if (currentChar>=len(elem)):
-			break
+    def lsb(currentlyCodedElem, currentBit, to_byte=ord):
+        code = to_byte(currentlyCodedElem)
+        bitToCode = code & (1 << currentBit)
+        return bitToCode >> currentBit
 
-		r,g,b = pix
+    currentChar = 0
+    currentBit = 0
+    count = 0
 
-		r = r&254
-		r |= lsb(elem[currentChar], currentBit)
-		nextChar()
-		if (currentChar<len(elem)):
-			g = g&254
-			g |= lsb(elem[currentChar], currentBit)
-			nextChar()
+    def nextChar():
+        nonlocal currentBit, currentChar, pixel_arr
+        if currentBit == 7:
+            currentBit = 0
+            currentChar += 1
+        else:
+            currentBit += 1
 
-		if (currentChar<len(elem)):
-			b = b&254
-			b |= lsb(elem[currentChar], currentBit)
-			nextChar()
+    for pix in data:
+        if currentChar >= len(elem):
+            break
 
-		pixel_arr.append((r,g,b))
-	return pixel_arr
+        r, g, b = pix
+
+        r = r & 254
+        r |= lsb(elem[currentChar], currentBit)
+        nextChar()
+        if currentChar < len(elem):
+            g = g & 254
+            g |= lsb(elem[currentChar], currentBit)
+            nextChar()
+
+        if currentChar < len(elem):
+            b = b & 254
+            b |= lsb(elem[currentChar], currentBit)
+            nextChar()
+
+        pixel_arr.append((r, g, b))
+    return pixel_arr
 
 
 def putPixelsByOneByte(data, elem):
-	"""
-	в один пиксель один значащий байт
-	"""
-	currentChar = 0
-	count = 0
-	pixel_arr = []
-	for pix in data:
-		if not (currentChar<len(elem)):
-			break
-		r,g,b = pix
-		currentlyCodedElem = elem[currentChar]
-		code = ord(currentlyCodedElem)
-		threeBits = 0b11111000
-		twoBits = 0b11111100
-		r = (r & threeBits) | (code & ~threeBits)
-		g = (g & threeBits) | (code>>3 & ~threeBits)
-		b = (b & twoBits)   | (code>>6 & ~twoBits)
-		currentChar += 1
-		pixel_arr.append((r,g,b))
+    """
+    в один пиксель один значащий байт
+    """
+    currentChar = 0
+    count = 0
+    pixel_arr = []
+    for pix in data:
+        if not (currentChar < len(elem)):
+            break
+        r, g, b = pix
+        currentlyCodedElem = elem[currentChar]
+        code = ord(currentlyCodedElem)
+        threeBits = 0b11111000
+        twoBits = 0b11111100
+        r = (r & threeBits) | (code & ~threeBits)
+        g = (g & threeBits) | (code >> 3 & ~threeBits)
+        b = (b & twoBits) | (code >> 6 & ~twoBits)
+        currentChar += 1
+        pixel_arr.append((r, g, b))
 
-	return pixel_arr
-		
+    return pixel_arr
+
+
 def changeFilename(imgPath, innerString, extension=".png"):
-	#imgPath.split(".")[-1]
-	return os.path.splitext(imgPath)[0] + innerString + extension							
-
-def encrypt(imgPath: str, secretInfo: str, outputPath: str, mode="1b"):	
-	img = Image.open(imgPath)
-	img.show()
-
-	draw = ImageDraw.Draw(img)	   		
-	pixs = img.load()
-
-	width = img.size[0]  		   	
-	height = img.size[1]
-
-	# кидаем данные в одномерный массив
-	input_data = [pixs[(x,y)] for x in range(width) for y in range(height)]
-	
-	if mode == "1b":
-		output = putPixelsByOneBit(input_data, secretInfo)
-	elif mode == "1B":
-		output = putPixelsByOneByte(input_data, secretInfo)
-	else:
-		output = putPixelsByThreeBit(input_data, secretInfo)
-
-	x, y = 0, 0
-	for i in range(len(output)):
-		draw.point((x,y), output[i]) # магия
-		y += 1
-		if y >= height:
-			y = 0
-			x += 1
-			# здесь должна быть проверка на выход за границы дозволенного, но мне влом
-
-	if not outputPath:
-		outputPath = changeFilename(imgPath, "coded")
-	
-	img.save(outputPath, "PNG")
-	img.show()
+    # imgPath.split(".")[-1]
+    return os.path.splitext(imgPath)[0] + innerString + extension
 
 
+def encrypt(imgPath: str, secretInfo: str, outputPath: str, mode="1b"):
+    img = Image.open(imgPath)
+    img.show()
+
+    draw = ImageDraw.Draw(img)
+    pixs = img.load()
+
+    width = img.size[0]
+    height = img.size[1]
+
+    # кидаем данные в одномерный массив
+    input_data = [pixs[(x, y)] for x in range(width) for y in range(height)]
+
+    if mode == "1b":
+        output = putPixelsByOneBit(input_data, secretInfo)
+    elif mode == "1B":
+        output = putPixelsByOneByte(input_data, secretInfo)
+    else:
+        output = putPixelsByThreeBit(input_data, secretInfo)
+
+    x, y = 0, 0
+    for i in range(len(output)):
+        draw.point((x, y), output[i])  # магия
+        y += 1
+        if y >= height:
+            y = 0
+            x += 1
+        # здесь должна быть проверка на выход за границы дозволенного, но мне влом
+
+    if not outputPath:
+        outputPath = changeFilename(imgPath, "coded")
+
+    img.save(outputPath, "PNG")
+    img.show()
