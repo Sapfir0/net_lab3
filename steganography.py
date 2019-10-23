a3e9086a0bbfa9e31313d3256ed8648ba0262c2f
Sapfir0 <sapfir999999@yandex.ru>
2019-10-23 11:41:45 +0400

    фикс

--- steganography.py
+++ steganography.py
@@ -14,7 +14,7 @@ parser.add_argument('-e', '--encrypt', action="store_true",)
 parser.add_argument('-i', '--imgPath', default=inputImgPath )
 parser.add_argument('-m', '--mode', default="1b", type=str, help="Input 1b or 1bit for coding 1 bit \n 3b or 3bit for coding 3 bit \n 1B or 1Byte for coding full one byte to pixel")
 parser.add_argument('-s', '--story', default=secretInfo, type=str, help="Input you story")
-parser.add_argument('--length', default=len(secretInfo), type=int, help="Input decrypt length > 0")
+parser.add_argument('--length', default=-1, type=int, help="Input decrypt length > 0")
 
 
 args = parser.parse_args()
@@ -22,8 +22,8 @@ algorithm = args.mode
 
 
 if args.decrypt:
-    length = args.length if args.length > 0 else 10
-    res = decrypt(args.imgPath, args.length, algorithm)
+    length = args.length if args.length > 0 else len(secretInfo)
+    res = decrypt(args.imgPath, length, algorithm)
     print(f"Раcшифрована информация с файла {args.imgPath}:\n{res}")
 elif args.encrypt:
     encrypt(args.imgPath, args.story, outputImgPath, algorithm)
